ecacb837b8659b81a9b31ac62d8091307874c6fc
Farhan531300 <azeemrehamani786786@gmail.com>
2025-09-16 18:20:20 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 const imageList = [
-    "my-design-1.jpg",
-    "customer-logo.png", 
-    "special-order.jpeg"
-    // Add more as needed
+    "IMG_20250803150025.jpg",
+    "IMG_20250207_143650.jpg",
+    "IMG_20250608_123023.jpg",
+    "IMG_20250803_153045.jpg"
 ];
